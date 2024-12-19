641451c1a0a674033abeb0ba792340ef5c071c3e
Worrapon <nine6805@gmail.com>
2024-12-19 14:59:01 +0700

    feat(core): adjust edit page & card a little bit

--- src/app/data/mockModules.ts
+++ src/app/data/mockModules.ts
@@ -5,21 +5,21 @@ export const mockModules = [
       title: "Introduction to Penetration Testing",
       description: "Learn the basics of penetration testing and ethical hacking.",
       pdfUrl: "https://eledu.ssru.ac.th/thanatyod_ja/pluginfile.php/1761/block_html/content/PrintMath2560%20-Edit.pdf",
-      image: "/image/path",
+      image: "https://img.freepik.com/free-vector/futuristic-technological-wallpaper_79603-1093.jpg",
     },
     {
       id: 2,
       title: "Web Application Security",
       description: "Deep dive into web application security vulnerabilities and exploits.",
       pdfUrl: "/path/to/web-application-security.pdf",
-      image: "/image/path",
+      image: "https://img.freepik.com/free-vector/futuristic-technological-wallpaper_79603-1093.jpg",
     },
     {
       id: 3,
       title: "Advanced Exploitation Techniques",
       description: "Master advanced techniques for exploiting systems and applications.",
       pdfUrl: "/path/to/advanced-exploitation-techniques.pdf",
-      image: "/image/path",
+      image: "https://img.freepik.com/free-vector/futuristic-technological-wallpaper_79603-1093.jpg",
     },
   ];
 
