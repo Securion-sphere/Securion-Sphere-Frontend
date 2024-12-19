dd104049ccec17001f4b1389cc6cbb493172d736
Worrapon <nine6805@gmail.com>
2024-12-19 15:23:13 +0700

    feat(core): create learning module page for student

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -70,6 +70,14 @@ export default function NavBar() {
           </Link>
         ))}
 
+        {/* Admin Panel Button */}
+        <Link
+          href="/monitor/dashboard"
+          className="inline-flex items-center px-4 py-2 text-sm font-medium text-white bg-blue-500 rounded-xl hover:bg-blue-600 focus:outline-none"
+        >
+          Admin Panel
+        </Link>
+
         {/* Profile Dropdown */}
         <div className="relative">
           <button
